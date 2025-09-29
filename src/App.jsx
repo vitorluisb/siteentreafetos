6ad911523e2b29290567c11facaacb91efdd6d11
Vitor Luis <vitorluisb@gmail.com>
2025-09-29 07:06:23 -0300

    Backup: Save current version of Entre Afetos website - 2025-09-29 07:06

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Chatbot from './components/Chatbot';
@@ -12,24 +12,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/cartao" element={<BusinessCardPage />} />
-        <Route path="/*" element={
-          <>
-            <Navigation />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sobre" element={<About />} />
-              <Route path="/servicos" element={<Services />} />
-              <Route path="/contato" element={<Contact />} />
-            </Routes>
-            <Footer />
-            <Chatbot />
-          </>
-        } />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/cartao" element={<BusinessCardPage />} />
+          <Route path="/*" element={
+            <>
+              <Navigation />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sobre" element={<About />} />
+                <Route path="/servicos" element={<Services />} />
+                <Route path="/contato" element={<Contact />} />
+              </Routes>
+              <Footer />
+              <Chatbot />
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
